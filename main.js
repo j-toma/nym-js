a5ca084cae4ba948e62e34c9fe9a086822f695b8
j-toma <j-toma@github.com>
2023-09-10 12:28:00 -0700

    testing git on lappy

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ import { doc_view } from './modules/initialSetup.js';
 import { createStartingTemplate } from './modules/newDocument.js';
 import * as viewChildCounter from './modules/counter.js';
 
-console.log(viewChildCounter.viewChildCounter);
+//console.log(viewChildCounter.viewChildCounter);
 createStartingTemplate(doc_view);
 console.log(viewChildCounter.viewChildCounter);
 
