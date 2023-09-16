d0a5ec329d290f5b724bba651f1318286563d69c
j-toma <j-toma@github.com>
2023-09-16 16:50:59 -0700

    testing new token

---
+++ modules/newElementToggle.js
@@ -0,0 +1,6 @@
+// this should be present on the view element div
+// if on, you it should clear the input box
+// if off, if should set the input to the value of element clicked
+// the submit button should respect the dropdown if toggled on
+
+
