40e2758d0ef77808f562afbfad4ec1e2c3f6021f
j-toma <j-toma@github.com>
2023-09-16 16:57:17 -0700

    trying again

--- modules/newElementToggle.js
+++ modules/newElementToggle.js
@@ -2,5 +2,6 @@
 // if on, you it should clear the input box
 // if off, if should set the input to the value of element clicked
 // the submit button should respect the dropdown if toggled on
+// (it should replace if off, add after if on)
 
 
